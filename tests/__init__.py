eeb8ca81de26dc0292530d81ee899436f56b668b
Paolo Bonzini <pbonzini@redhat.com>
2022-04-22 17:20:16 +0200

    tests for callers and callees

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
 import unittest
 import vrc
 
+
 class VRCGraphTest(unittest.TestCase):
     def test_edge_to_nonexisting_node(self):
         """Check creating an edge to a function that is not defined."""
@@ -61,3 +62,65 @@ class VRCGraphTest(unittest.TestCase):
         graph.omit_node("a")
         graph.omit_node("b")
         self.assertTrue(graph.filter_edge("b", "c", False))
+
+    def test_callers(self):
+        """Check callers(), "call" edges only."""
+        graph = vrc.Graph()
+        graph.add_node("a")
+        graph.add_node("b")
+        graph.add_node("c")
+        graph.add_node("d")
+        graph.add_edge("a", "c", "call")
+        graph.add_edge("b", "c", "call")
+        graph.add_edge("c", "d", "call")
+        self.assertEqual(sorted(graph.callers("c", False)), ["a", "b"])
+
+    def test_callers_ref_ok(self):
+        """Check callers(), "call" and "ref"."""
+        graph = vrc.Graph()
+        graph.add_node("a")
+        graph.add_node("b")
+        graph.add_node("c")
+        graph.add_node("d")
+        graph.add_edge("a", "c", "ref")
+        graph.add_edge("b", "c", "call")
+        graph.add_edge("c", "d", "call")
+        self.assertEqual(sorted(graph.callers("c", True)), ["a", "b"])
+        self.assertEqual(sorted(graph.callers("c", False)), ["b"])
+
+    def test_callees(self):
+        """Check callees(), "call" edges to non-external nodes."""
+        graph = vrc.Graph()
+        graph.add_node("a")
+        graph.add_node("b")
+        graph.add_node("c")
+        graph.add_node("d")
+        graph.add_edge("a", "b", "call")
+        graph.add_edge("b", "c", "call")
+        graph.add_edge("b", "d", "call")
+        self.assertEqual(sorted(graph.callees("b", False, False)), ["c", "d"])
+
+    def test_callees_ref_ok(self):
+        """Check callees(), "call" and "ref" edges."""
+        graph = vrc.Graph()
+        graph.add_node("a")
+        graph.add_node("b")
+        graph.add_node("c")
+        graph.add_node("d")
+        graph.add_edge("a", "b", "call")
+        graph.add_edge("b", "c", "call")
+        graph.add_edge("b", "d", "ref")
+        self.assertEqual(sorted(graph.callees("b", False, True)), ["c", "d"])
+        self.assertEqual(sorted(graph.callees("b", False, False)), ["c"])
+
+    def test_callees_external_ok(self):
+        """Check callees() for external nodes."""
+        graph = vrc.Graph()
+        graph.add_node("a")
+        graph.add_node("b")
+        graph.add_node("c")
+        graph.add_edge("a", "b", "call")
+        graph.add_edge("b", "c", "call")
+        graph.add_edge("b", "d", "call")
+        self.assertEqual(sorted(graph.callees("b", True, False)), ["c", "d"])
+        self.assertEqual(sorted(graph.callees("b", False, False)), ["c"])
