62fc867798fe845784c227a31d5625ee446e9c2b
Paolo Bonzini <pbonzini@redhat.com>
2022-11-10 13:25:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='vrc',
-    version='1.0',
+    version='1.0.99',
     description='Call graph explorer tool',
     long_description=long_description,
     long_description_content_type='text/markdown',
