f9815bad549ff5892b850a7ccf543d262eb0987b
Paolo Bonzini <pbonzini@redhat.com>
2022-11-25 13:35:21 +0100

    move setuptools configuration to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from setuptools import setup
+import setuptools
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
-
-setup(
-    name='vrc',
-    version='1.0.99',
-    description='Call graph explorer tool',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Paolo Bonzini',
-    author_email='bonzini@gnu.org',
-    packages=['vrc', 'vrc.automata', 'vrc.cli', 'vrc.loaders'],
-    install_requires=[
-        'compynator'
-    ],
-    entry_points={
-        'console_scripts': [
-            'vrc = vrc.cli.main:main',
-        ]
-    }
-)
+if __name__ == "__main__":
+    setuptools.setup()
