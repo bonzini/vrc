1ede449d72b0abec8f5ac0cd07010428149feb54
Paolo Bonzini <pbonzini@redhat.com>
2022-12-06 10:55:24 +0100

    use C graph in cython_graph
    Replace the dummy implementation of cython_graph with Python bindings
    to the cgraph.cc functions.

---
+++ vrc/cgraph.pxd
@@ -0,0 +1,48 @@
+"""Cython description of graph.h."""
+
+from libcpp cimport bool as c_bool
+
+cdef extern from "cgraph.h":
+    ctypedef struct RCUThread:
+        pass
+
+    ctypedef struct Graph:
+        pass
+
+    ctypedef struct Iter:
+        pass
+
+    Graph *graph_new() nogil;
+    void graph_delete(RCUThread *rcu, Graph *g);
+
+    size_t graph_add_external_node(RCUThread *rcu, Graph *g, const char *name);
+    void graph_set_defined(RCUThread *rcu, Graph *g, size_t i);
+    void graph_set_username(RCUThread *rcu, Graph *g, size_t i, const char *username);
+    void graph_set_location(RCUThread *rcu, Graph *g, size_t i, const char *file, ssize_t line);
+    void graph_add_edge(RCUThread *rcu, Graph *g, size_t caller, size_t callee, c_bool is_call);
+
+    size_t graph_node_count(RCUThread *rcu, Graph *g);
+    char *graph_name_by_index(RCUThread *rcu, Graph *g, size_t i);
+    char *graph_location_by_index(RCUThread *rcu, Graph *g, size_t i, ssize_t *line);
+    char *graph_username_by_index(RCUThread *rcu, Graph *g, size_t i);
+    ptrdiff_t graph_get_node(RCUThread *rcu, Graph *g, const char *name);
+    c_bool graph_is_node_external(RCUThread *rcu, Graph *g, size_t i);
+    c_bool graph_has_edge(RCUThread *rcu, Graph *g, size_t src, size_t dest, c_bool ref_ok);
+    c_bool graph_has_call_edge(RCUThread *rcu, Graph *g, size_t src, size_t dest);
+
+    Iter *graph_get_callers(RCUThread *rcu, Graph *g, size_t i);
+    Iter *graph_get_callees(RCUThread *rcu, Graph *g, size_t i);
+    Iter *graph_get_refs(RCUThread *rcu, Graph *g, size_t i);
+    Iter *graph_all_nodes_for_file(RCUThread *rcu, Graph *g, const char *str);
+    Iter *graph_all_nodes_for_label(RCUThread *rcu, Graph *g, const char *str);
+
+    void graph_add_label(RCUThread *rcu, Graph *g, size_t i, const char *label);
+    c_bool graph_has_label(RCUThread *rcu, Graph *g, size_t i, const char *label);
+
+    const char **graph_all_files(RCUThread *rcu, Graph *g, size_t *n);
+    const char **graph_all_labels(RCUThread *rcu, Graph *g, size_t *n);
+
+    void graph_reset_labels(Graph *g);
+
+    void iter_delete(Iter *i);
+    ptrdiff_t iter_next(Iter *i);
--- vrc/cython_graph.pyx
+++ vrc/cython_graph.pyx
@@ -9,27 +9,102 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-from collections import defaultdict
 import dataclasses
 import os
 import typing
 
+from libcpp cimport bool as c_bool
+from libc.stdlib cimport free
+cimport vrc.cgraph as cgraph
 
-@dataclasses.dataclass
-class Node:
-    name: str
-    callers: set[int]
-    callees: dict[int, bool]
-    username: typing.Optional[str] = None
-    file: typing.Optional[str] = None
-    line: typing.Optional[int] = None
-    external: bool = True
 
-    def __init__(self, name: str) -> None:
-        super().__init__()
-        self.name = name
-        self.callers = set()
-        self.callees = dict()
+cdef extern from "Python.h":
+    char* PyUnicode_AsUTF8(object unicode)
+
+cdef class Iter:
+    cdef cgraph.Iter *i
+
+    def __dealloc__(self):
+        cgraph.iter_delete(self.i)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        n = -1
+        if self.i:
+            n = cgraph.iter_next(self.i)
+            if n == -1:
+                cgraph.iter_delete(self.i)
+                self.i = NULL
+        if n == -1:
+            raise StopIteration
+        return n
+
+
+cdef object iter_to_python(cgraph.Iter *i):
+    it = Iter()
+    it.i = i
+    return it
+
+
+cdef object c_string_decode(char *s):
+    result = s.decode("utf-8")
+    free(s)
+    return result
+
+
+cdef object c_strings_to_list(char **s, int n):
+    result = list()
+    for i in range(0, n):
+        result.append(s[i].decode("utf-8"))
+        free(s[i])
+    free(s)
+    return result
+
+
+cdef class Node:
+    cdef cgraph.Graph *g
+    cdef size_t index
+    cdef object _name
+    cdef object _username
+    cdef object _file
+    cdef object _line      # integer or None
+    cdef c_bool _external
+
+    def __cinit__(self) -> None:
+        self._external = True
+
+    @property
+    def name(self) -> typing.Optional[str]:
+        if self._name is None:
+            self._name = c_string_decode(cgraph.graph_name_by_index(NULL, self.g, self.index))
+            self._username = c_string_decode(cgraph.graph_username_by_index(NULL, self.g, self.index))
+        return self._name or None
+
+    @property
+    def username(self) -> typing.Optional[str]:
+        if self._name is None:
+            self.name
+        return self._username or None
+
+    @property
+    def file(self) -> typing.Optional[str]:
+        cdef ssize_t line
+        if self._line is None:
+            self._file = c_string_decode(cgraph.graph_location_by_index(NULL, self.g, self.index, &line))
+            self._line = line
+        return self._file or None
+
+    @property
+    def line(self) -> typing.Optional[str]:
+        if self._line is None:
+            self.file
+        return None if self._line == -1 else self._line
+
+    @property
+    def external(self) -> bool:
+        return self._external
 
     def format(self, include_location: bool) -> str:
         n = self.username or self.name
@@ -42,124 +117,124 @@ class Node:
             return f"{n} ({file}:{self.line})"
 
 
-class Graph:
-    nodes_by_index: list[Node]
-    nodes: dict[str, int]
-    nodes_by_username: dict[str, int]
-    nodes_by_file: dict[str, list[int]]
-    node_labels: dict[str, set[int]]
+cdef class Graph:
+    cdef cgraph.Graph *g
+    cdef list nodes_by_index
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __cinit__(self) -> None:
+        self.g = cgraph.graph_new()
         self.nodes_by_index = []
-        self.nodes = {}
-        self.nodes_by_username = {}
-        self.nodes_by_file = defaultdict(lambda: list())
-
-        self.reset_labels()
 
     def add_external_node(self, name: str) -> int:
-        i = self.nodes_by_username.get(name, None)
-        if i is not None:
-            return i
-
-        i = self.nodes.get(name, None)
-        if i is not None:
-            return i
-
-        i = len(self.nodes_by_index)
-        self.nodes[name] = i
-        self.nodes_by_index.append(Node(name=name))
-        return i
+        cdef char *string = PyUnicode_AsUTF8(name)
+        return cgraph.graph_add_external_node(NULL, self.g, string)
 
     def _add_node(self, i: int, username: typing.Optional[str] = None,
                   file: typing.Optional[str] = None,
                   line: typing.Optional[int] = None) -> None:
-        node = self.nodes_by_index[i]
-        if node.external:
+        cdef char *string
+        cdef Node node
+        if cgraph.graph_is_node_external(NULL, self.g, i):
             # This is now a defined node.  It might have a username and a file
-            node.username = username
-            node.file = file
-            node.line = line
-            node.external = False
+            cgraph.graph_set_defined(NULL, self.g, i)
+            node = self._node_by_index(i)
+            node._external = False
+            node.name
             if username:
-                self.nodes_by_username[username] = i
+                node._username = username
+                string = PyUnicode_AsUTF8(username)
+                cgraph.graph_set_username(NULL, self.g, i, string)
             if file:
-                self.nodes_by_file[file].append(i)
+                node._file = file
+                node._line = -1 if line is None else line
+                string = PyUnicode_AsUTF8(file)
+                cgraph.graph_set_location(NULL, self.g, i, string, <ssize_t>node._line)
 
     def _add_edge(self, i: int, j: int, is_call: bool) -> None:
-        src = self.nodes_by_index[i]
-        # A "ref" edge does not override a "call" edge
-        if is_call or j not in src.callees:
-            src.callees[j] = is_call
-
-        self.nodes_by_index[j].callers.add(i)
+        cgraph.graph_add_edge(NULL, self.g, i, j, is_call)
 
     def node_count(self) -> int:
-        return len(self.nodes_by_index)
+        return cgraph.graph_node_count(NULL, self.g)
 
     def _node_by_index(self, i: int) -> Node:
+        cdef Node node
+        if len(self.nodes_by_index) <= i:
+            if i >= self.node_count():
+                raise IndexError
+            self.nodes_by_index += [None] * (self.node_count() - len(self.nodes_by_index) + 1)
+        if self.nodes_by_index[i] is None:
+            node = Node()
+            node.g = self.g
+            node.index = i
+            self.nodes_by_index[i] = node
         return self.nodes_by_index[i]
 
+    def _username_by_index(self, i: int) -> str:
+        return self._node_by_index(i).username
+
     def _name_by_index(self, i: int) -> str:
-        node = self.nodes_by_index[i]
-        return node.username or node.name
+        return self._node_by_index(i).name
 
     def _get_callers(self, i: int) -> typing.Iterable[int]:
-        return self.nodes_by_index[i].callers
+        return iter_to_python(cgraph.graph_get_callers(NULL, self.g, i))
 
     def _get_callees(self, i: int) -> typing.Iterable[int]:
-        return self.nodes_by_index[i].callees.keys()
+        yield from iter_to_python(cgraph.graph_get_callees(NULL, self.g, i))
+        yield from iter_to_python(cgraph.graph_get_refs(NULL, self.g, i))
 
     def _get_node(self, name: str) -> typing.Union[typing.Tuple[None, None], typing.Tuple[int, str]]:
-        i = self.nodes_by_username.get(name, None)
-        if i is not None:
-            return i, name
+        cdef char *string = PyUnicode_AsUTF8(name)
+        cdef ssize_t result = cgraph.graph_get_node(NULL, self.g, string)
+        if result == -1:
+            return None, None
 
-        i = self.nodes.get(name, None)
-        if i is not None:
-            node = self.nodes_by_index[i]
-            return i, node.username or name
-        return None, None
+        n = self._node_by_index(result)
+        return result, n.username or name
 
     def _is_node_external(self, i: int) -> bool:
-        return self.nodes_by_index[i].external
+        return cgraph.graph_is_node_external(NULL, self.g, i)
 
     def _has_edge(self, src: int, dest: int, ref_ok: bool) -> bool:
-        e = self.nodes_by_index[src].callees.get(dest, None)
-        if e is not False:
-            # nonexistant or call edge
-            return e is True
-        # ref edge
-        return ref_ok and not self.nodes_by_index[dest].external
+        return cgraph.graph_has_edge(NULL, self.g, src, dest, ref_ok)
 
     def _has_call_edge(self, src: int, dest: int) -> bool:
-        return self.nodes_by_index[src].callees.get(dest, False)
+        return cgraph.graph_has_call_edge(NULL, self.g, src, dest)
 
     def all_files(self) -> typing.Iterable[str]:
-        return iter(self.nodes_by_file.keys())
+        cdef size_t n
+        cdef const char **strings = cgraph.graph_all_files(NULL, self.g, &n)
+        return c_strings_to_list(strings, n)
 
     def _all_nodes(self) -> typing.Iterable[int]:
-        return range(0, self.node_count())
+        return range(0, cgraph.graph_node_count(NULL, self.g))
 
     def _all_nodes_for_file(self, file: str) -> typing.Iterable[int]:
-        return self.nodes_by_file[file]
+        return iter_to_python(cgraph.graph_all_nodes_for_file(NULL, self.g, file.encode("utf-8")))
 
     def name(self, x: str) -> str:
-        n = self.nodes_by_index[self.nodes[x]]
+        cdef char *string = PyUnicode_AsUTF8(x)
+        cdef ssize_t result = cgraph.graph_get_node(NULL, self.g, string)
+        if result == -1:
+            raise KeyError(x)
+
+        n = self._node_by_index(result)
         return n.username or x
 
     def labels(self) -> typing.Iterable[str]:
-        return self.node_labels.keys()
+        cdef size_t n
+        cdef const char **strings = cgraph.graph_all_labels(NULL, self.g, &n)
+        return c_strings_to_list(strings, n)
 
     def _all_nodes_for_label(self, label: str) -> typing.Iterable[int]:
-        return self.node_labels[label]
+        return iter_to_python(cgraph.graph_all_nodes_for_label(NULL, self.g, label.encode("utf-8")))
 
     def _add_label(self, i: int, label: str) -> None:
-        self.node_labels[label].add(i)
+        cdef char *string = PyUnicode_AsUTF8(label)
+        cgraph.graph_add_label(NULL, self.g, i, string)
 
     def _has_label(self, i: int, label: str) -> bool:
-        return (label in self.node_labels) and (i in self.node_labels[label])
+        cdef char *string = PyUnicode_AsUTF8(label)
+        return cgraph.graph_has_label(NULL, self.g, i, string)
 
     def reset_labels(self) -> None:
-        self.node_labels = defaultdict(lambda: set())
+        cgraph.graph_reset_labels(self.g)
